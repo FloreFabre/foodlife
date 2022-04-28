5568bcb1b90a41b15969428e5233e8268f2e1518
Joaquim410 <j.willsmith974@gmail.com>
2022-04-28 15:46:29 +0400

    création de la nav-bar et footer

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+
+  <title>FoodLife</title>
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="./css/settings.css" />
+  <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+  <header>
+  <nav class="navbar">
+ <!-- LOGO -->
+ <div class="logo"><img src="./img/Foodieland.png" alt="" width="110px"></div>
+ <!-- NAVIGATION MENU -->
+ <ul class="nav-links">
+ <!-- USING CHECKBOX HACK -->
+ <input type="checkbox" id="checkbox_toggle" />
+ <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+ <!-- NAVIGATION MENUS -->
+ <div class="menu">
+ <li><a href="/">Home</a></li>
+ <li><a href="/">Blog</a></li>
+ <li class="services">
+ <a href="/">Recipes</a>
+ <!-- DROPDOWN MENU -->
+ <ul class="dropdown">
+ <li><a href="/">Dropdown 1 </a></li>
+ <li><a href="/">Dropdown 2</a></li>
+ <li><a href="/">Dropdown 2</a></li>
+ <li><a href="/">Dropdown 3</a></li>
+ <li><a href="/">Dropdown 4</a></li>
+ </ul>
+ </li>
+ <li><a href="/">Contact</a></li>
+ <li><a href="/">About us</a></li>
+ </div>
+ </ul>
+ <div class="headerlogo">
+      <div class="lg2"><a href="https://fr-fr.facebook.com/"><i class="fa-brands fa-facebook-f fa-2x"></i></div></a>
+      <div class="lg2"><a href="https://twitter.com/?lang=fr"><i class="fa-brands fa-twitter fa-2x"></i></div></a>
+      <div class="lg2"><a href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-2x"></i></div></a>
+ </div>
+ </nav>
+ <hr class="un">
+
+  </header>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <footer>
+    <div class="footer">
+      <div class="logo">
+          <img src="./img/Foodieland.png" alt="" width="110px">
+    <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, </p>
+    </div>
+     <div class="menuf">
+      <div class="recipes"> <a href="index.php">Recipes</a></div>
+      <div class="blog"><a href="listeArticles.php">Blog </a></div>
+      <div class="contact"> <a href="login_index.php">Contact</a></div>
+      <div class="aboutus"> <a href="login_index.php">About us</a></div>
+    </div>
+    </div>
+    <hr class="footerhr">
+    <div class="footer2"> 
+        <p>© 2020 Flowbase. Powered by <a href="">Webflow</a></p>
+        <div class="resouxf">
+    <div class="lg2"><a href="https://fr-fr.facebook.com/"><i class="fa-brands fa-facebook-f fa-2x"></i></div></a>
+      <div class="lg2"><a href="https://twitter.com/?lang=fr"><i class="fa-brands fa-twitter fa-2x"></i></div></a>
+      <div class="lg2"><a href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-2x"></i></div></a>
+  </div>  
+</div>
+  </footer>
+
+</body>
+
+</html>
