e8689e96a09ce026122850401359f66492315728
Joaquim410 <j.willsmith974@gmail.com>
2022-04-29 15:53:57 +0400

    ccc

--- index.php
+++ index.php
@@ -13,49 +13,96 @@
 
 <body>
   <header>
-  <nav class="navbar">
- <!-- LOGO -->
- <div class="logo"><img src="./img/Foodieland.png" alt="" width="110px"></div>
- <!-- NAVIGATION MENU -->
- <ul class="nav-links">
- <!-- USING CHECKBOX HACK -->
- <input type="checkbox" id="checkbox_toggle" />
- <label for="checkbox_toggle" class="hamburger">&#9776;</label>
- <!-- NAVIGATION MENUS -->
- <div class="menu">
- <li><a href="/">Home</a></li>
- <li><a href="/">Blog</a></li>
- <li class="services">
- <a href="/">Recipes</a>
- <!-- DROPDOWN MENU -->
- <ul class="dropdown">
- <li><a href="/">Dropdown 1 </a></li>
- <li><a href="/">Dropdown 2</a></li>
- <li><a href="/">Dropdown 2</a></li>
- <li><a href="/">Dropdown 3</a></li>
- <li><a href="/">Dropdown 4</a></li>
- </ul>
- </li>
- <li><a href="/">Contact</a></li>
- <li><a href="/">About us</a></li>
- </div>
- </ul>
- <div class="headerlogo">
-      <div class="lg2"><a href="https://fr-fr.facebook.com/"><i class="fa-brands fa-facebook-f fa-2x"></i></div></a>
-      <div class="lg2"><a href="https://twitter.com/?lang=fr"><i class="fa-brands fa-twitter fa-2x"></i></div></a>
-      <div class="lg2"><a href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-2x"></i></div></a>
- </div>
- </nav>
- <hr class="un">
+    <nav class="navbar">
+      <!-- LOGO -->
+      <div class="logo"><img src="./img/Foodieland.png" alt="" width="110px"></div>
+      <!-- NAVIGATION MENU -->
+      <ul class="nav-links">
+        <!-- USING CHECKBOX HACK -->
+        <input type="checkbox" id="checkbox_toggle" />
+        <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+        <!-- NAVIGATION MENUS -->
+        <div class="menu">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Blog</a></li>
+          <li class="services">
+            <a href="/">Recipes</a>
+            <!-- DROPDOWN MENU -->
+            <ul class="dropdown">
+              <li><a href="/">Dropdown 1</a></li>
+              <li><a href="/">Dropdown 2</a></li>
+              <li><a href="/">Dropdown 2</a></li>
+              <li><a href="/">Dropdown 3</a></li>
+              <li><a href="/">Dropdown 4</a></li>
+            </ul>
+          </li>
+          <li><a href="/">Contact</a></li>
+          <li><a href="/">About us</a></li>
+        </div>
+      </ul>
+      <div class="headerlogo">
+        <div class="lg2"><a href="https://fr-fr.facebook.com/"><i class="fa-brands fa-facebook-f fa-2x"></i></div></a>
+        <div class="lg2"><a href="https://twitter.com/?lang=fr"><i class="fa-brands fa-twitter fa-2x"></i></div></a>
+        <div class="lg2"><a href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-2x"></i></div></a>
+      </div>
+    </nav>
+    <hr class="un">
 
   </header>
-  
+  <div class="grid">
+    <div class="recettedroite">
+     <img class="imgD" src="./img/chickenwings.png" alt="" >
+    </div>
+
+
+
+
+    <div class="recettegauche">
+
+  <img class="imgG" src="./img/Rectangle.png" alt="" >
+  <div class="HOT">
+  <img class="imgHOT" src="./img/image14.png" alt="" >
+<span class="hotrecette"> Hot Recipes</span>
+</div>
+
+  <span class="titreR"> Spicy delicious chicken wings</span>
+  <span class="texteR">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
+
+ <div class="time">
+ <img class="imgTime" src="./img/Vector.png" alt="" >
+ <span class="min"> 30 Minutes</span>
+ </div>
+ <div class="catég">
+
+   <div class="imgCat">
+ <img class="" src="./img/Vector (1).png" alt="" >
+ <img class="" src="./img/Vector (2).png" alt="" >
+</div>
+ <span class="cat">Chicken</span>
+ </div>
+
+<div class="auteur">
+<img class="imgAuteur" src="./img/jhonsmith.png" alt="" >
+
+<div class="auteurNom">
+<span>John Smith</span>
+<span class="timeAuteur"> 15 March 2022</span>
+</div>
+</div>
+
+<div class="bouttonRecipe">
+<img class="imgBoutton" src="./img/bouttonplay.png" alt="" >
+<span class="view">View Recipes</span>
+</div>
 
 
 
 
 
 
+    </div>
+
+  </div>
 
 
 
@@ -67,25 +114,25 @@
   <footer>
     <div class="footer">
       <div class="logo">
-          <img src="./img/Foodieland.png" alt="" width="110px">
-    <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, </p>
-    </div>
-     <div class="menuf">
-      <div class="recipes"> <a href="index.php">Recipes</a></div>
-      <div class="blog"><a href="listeArticles.php">Blog </a></div>
-      <div class="contact"> <a href="login_index.php">Contact</a></div>
-      <div class="aboutus"> <a href="login_index.php">About us</a></div>
-    </div>
+        <img src="./img/Foodieland.png" alt="" width="110px">
+        <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, </p>
+      </div>
+      <div class="menuf">
+        <div class="recipes"> <a href="index.php">Recipes</a></div>
+        <div class="blog"><a href="listeArticles.php">Blog </a></div>
+        <div class="contact"> <a href="login_index.php">Contact</a></div>
+        <div class="aboutus"> <a href="login_index.php">About us</a></div>
+      </div>
     </div>
     <hr class="footerhr">
-    <div class="footer2"> 
-        <p>© 2020 Flowbase. Powered by <a href="">Webflow</a></p>
-        <div class="resouxf">
-    <div class="lg2"><a href="https://fr-fr.facebook.com/"><i class="fa-brands fa-facebook-f fa-2x"></i></div></a>
-      <div class="lg2"><a href="https://twitter.com/?lang=fr"><i class="fa-brands fa-twitter fa-2x"></i></div></a>
-      <div class="lg2"><a href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-2x"></i></div></a>
-  </div>  
-</div>
+    <div class="footer2">
+      <p>© 2020 Flowbase. Powered by <a href="">Webflow</a></p>
+      <div class="resouxf">
+        <div class="lg2"><a href="https://fr-fr.facebook.com/"><i class="fa-brands fa-facebook-f fa-2x"></i></div></a>
+        <div class="lg2"><a href="https://twitter.com/?lang=fr"><i class="fa-brands fa-twitter fa-2x"></i></div></a>
+        <div class="lg2"><a href="https://www.instagram.com/?hl=fr"><i class="fa-brands fa-instagram fa-2x"></i></div></a>
+      </div>
+    </div>
   </footer>
 
 </body>
