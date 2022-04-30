b84bc2be89f979ced98cfc770094417c6cfaead2
Joaquim410 <j.willsmith974@gmail.com>
2022-04-30 11:52:22 +0400

    mise a jour des recette et ajout de la section chef

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
   <title>FoodLife</title>
   <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="./css/recetteAccueil.css" />
   <link rel="stylesheet" href="./css/settings.css" />
   <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -145,10 +146,13 @@
     <!-- RECETTES -->
   </div>
 
+
+
+  <span class="simplyrecipes">Simple and tasty recipes</span>
+  <span class="simplyTexte">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
+
   <div class="recette">
 
-    <span class="simplyrecipes">Simple and tasty recipes</span>
-    <span class="simplyTexte">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
 
     <div class="recette1">
       <img class="imgR1" src="./img/burger.png" alt="">
@@ -157,27 +161,149 @@
       <img class="imgTimeR1" src="./img/Vector.png" alt="">
       <span class="minR1"> 30 Minutes</span>
       <div class="imgCatR1">
-          <img class="" src="./img/Vector (1).png" alt="">
-          <img class="" src="./img/Vector (2).png" alt="">
-        </div>
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
       <span class="catR1">Snack</span>
 
     </div>
 
 
+    <div class="recette2">
+      <img class="imgR2" src="./img/fish.png" alt="">
+      <img class="likeR2" src="./img/unlike.png" alt="">
+      <span class="recette2Texte"> Fresh Lime Roasted Salmon with Ginger Sauce</span>
+      <img class="imgTimeR2" src="./img/Vector.png" alt="">
+      <span class="minR2"> 30 Minutes</span>
+      <div class="imgCatR2">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR2">Fish</span>
+
+    </div>
+
+    <div class="recette3">
+      <img class="imgR3" src="./img/pancake.png" alt="">
+      <img class="likeR3" src="./img/unlike.png" alt="">
+      <span class="recette3Texte"> Strawberry Oatmeal Pancake with Honey Syrup</span>
+      <img class="imgTimeR3" src="./img/Vector.png" alt="">
+      <span class="minR3"> 30 Minutes</span>
+      <div class="imgCatR3">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR3">Breakfast</span>
+
+    </div>
+
+    <div class="recette4">
+      <img class="imgR4" src="./img/mixedmayo.png" alt="">
+      <img class="likeR4" src="./img/like.png" alt="">
+      <span class="recette4Texte"> Fresh and Healthy Mixed Mayonnaise Salad</span>
+      <img class="imgTimeR4" src="./img/Vector.png" alt="">
+      <span class="minR4"> 30 Minutes</span>
+      <div class="imgCatR4">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR4">Healthy</span>
+
+    </div>
+
+    <div class="recette5">
+
+      <img class="imgR5" src="./img/meatballs.png" alt="">
+      <img class="likeR5" src="./img/unlike.png" alt="">
+      <span class="recette5Texte"> Chicken Meatballs with Cream Cheese</span>
+      <img class="imgTimeR5" src="./img/Vector.png" alt="">
+      <span class="minR5"> 30 Minutes</span>
+      <div class="imgCatR5">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR5">Meat</span>
+
+    </div>
+
+    <div class="pub">
+
+      <span class="textpub">Don’t forget to eat
+        healthy food</span>
+      <img class="imgpub" src="./img/pubfood.png" alt="">
+      <img class="starpub" src="./img/pubstar.png" alt="">
+
+      <span class="lienpub">www.foodieland.com</span>
+
+
+    </div>
+
+    <div class="recette6">
+      <img class="imgR6" src="./img/orangepancake.png" alt="">
+      <img class="likeR6" src="./img/like.png" alt="">
+      <span class="recette6Texte"> Fruity Pancake with Orange & Blueberry</span>
+      <img class="imgTimeR6" src="./img/Vector.png" alt="">
+      <span class="minR6"> 30 Minutes</span>
+      <div class="imgCatR6">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR6">Sweet</span>
+
+    </div>
+
+    <div class="recette7">
+      <img class="imgR7" src="./img/chickenrice.png" alt="">
+      <img class="likeR7" src="./img/unlike.png" alt="">
+      <span class="recette7Texte">The Best Easy One Pot Chicken and Rice</span>
+      <img class="imgTimeR7" src="./img/Vector.png" alt="">
+      <span class="minR7"> 30 Minutes</span>
+      <div class="imgCatR7">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR7">Snack</span>
+
+    </div>
+
+
+    <div class="recette8">
+      <img class="imgR8" src="./img/creamychicken.png" alt="">
+      <img class="likeR8" src="./img/unlike.png" alt="">
+      <span class="recette8Texte">The Creamiest Creamy Chicken and Bacon Pasta</span>
+      <img class="imgTimeR8" src="./img/Vector.png" alt="">
+      <span class="minR8"> 30 Minutes</span>
+      <div class="imgCatR8">
+        <img class="" src="./img/Vector (1).png" alt="">
+        <img class="" src="./img/Vector (2).png" alt="">
+      </div>
+      <span class="catR8">Noodles</span>
+
+    </div>
+
+  </div>
 
 
 
 
+  <div class="chef">
+
+<span class="titrechef">Everyone can be a
+chef in their own kitchen</span>
+
+<span class="textechef">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
 
 
+<div class="chefdroite">
+<img class="imgchef" src="./img/portrait-happy-chef.png" alt="">
+
+<img class="imgchefT" src="./img/tomate.png" alt="">
+<img class="imgchefV" src="./img/cviande.png" alt="">
+<img class="imgchefO" src="./img/oignion.png" alt="">
+<img class="imgchefL" src="./img/cvegan.png" alt="">
 
 
-
-
-
-
-
+</div>
 
   </div>
 
